5c940d91fa0e16109166d17d404b9abf76af4f12
noricmk2 <noric123@gmail.com>
2018-01-30 09:05:00 +0900

    기본

---
+++ Assets/Scripts/Manager/BattleManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleManager : MonoSingleton<BattleManager>
+{
+
+    public void CoinSelect()
+    {
+
+    }
+
+    public void CoinBattle()
+    {
+    }
+}
---
+++ Assets/Scripts/Manager/MonoSingleton.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonoSingleton<T> : MonoBehaviour where T : MonoSingleton<T> 
+{
+    static bool shotdown = false;
+    private static T _instance = null;
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                if (shotdown == false)
+                {
+                    T instance = GameObject.FindObjectOfType<T>() as T;
+                    if (instance == null)
+                    {
+                        instance = new GameObject(typeof(T).ToString(), typeof(T)).GetComponent<T>();
+                    }
+
+                    InstanceInit(instance);
+                    Debug.Assert(_instance != null, typeof(T).ToString() + "singleton create fail");
+                }
+            }
+            return _instance;
+        }
+    }
+
+    private static void InstanceInit(Object inst)
+    {
+        _instance = inst as T;
+        _instance.Init();
+    }
+
+    public virtual void Init()
+    {
+        DontDestroyOnLoad(_instance);
+    }
+
+    private void OnDestroy()
+    {
+        _instance = null;
+    }
+
+    private void OnApplicationQuit()
+    {
+        _instance = null;
+        shotdown = true;
+    }
+}
