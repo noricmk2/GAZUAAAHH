838a62df97d900e8e2a77f139bfb0b12d5ffbe28
OhSeungWon <osw132@naver.com>
2018-02-06 14:24:38 +0900

    EffectManager, BaseEffect 추가

---
+++ Assets/Scripts/Effect/BaseEffect.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseEffect : BaseObject {
+
+    Transform trans;
+    public float playTime { get; set; }
+    float currTime;
+    GameObject effect;
+    public bool isEnd { get; set; }
+
+    public void Start()
+    {
+        isEnd = false;
+        currTime = 0;
+    }
+   
+    public override void CustomUpdate()
+    {
+        currTime += Time.deltaTime;
+        if (currTime > playTime)
+        {
+            isEnd = true;
+        }
+    }
+}
---
+++ Assets/Scripts/Manager/EffectManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectManager : MonoSingleton<EffectManager>
+{
+    List<BaseEffect> listEffect;
+
+    public void Awake()
+    {
+        listEffect = new List<BaseEffect>();
+    }
+
+    public void CreateEffect(Vector3 _position, float _playTime, string _path)
+    {        
+        GameObject effect = Instantiate(Resources.Load(_path) as GameObject);
+
+        effect.AddComponent<BaseEffect>();
+        effect.transform.position = _position;
+        BaseEffect baseEffect = effect.GetComponent<BaseEffect>();
+        baseEffect.playTime = _playTime;
+        
+        listEffect.Add(baseEffect);
+    }
+
+    public void Update()
+    {
+        List<int> tmplist = new List<int>();
+        foreach (BaseEffect effect in listEffect)
+        {
+            effect.CustomUpdate();
+            if(effect.isEnd)
+            {
+                tmplist.Add(listEffect.IndexOf(effect));
+                Destroy(effect.gameObject);
+            }
+        }
+        for (int i = listEffect.Count; i >= 0; i--)
+        {
+            foreach (int j in tmplist)
+            {
+                if (i == j)
+                {
+                    listEffect.RemoveAt(j);
+                }
+            }
+        }       
+    }
+}
