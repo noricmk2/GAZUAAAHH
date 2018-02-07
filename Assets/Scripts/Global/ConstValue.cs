998f557c8637c4ecca48867f3a7f05e9d1ca9b82
noricmk2 <noric123@gmail.com>
2018-02-07 15:47:40 +0900

    constvalue 추가

--- Assets/Scripts/Global/ConstValue.cs
+++ Assets/Scripts/Global/ConstValue.cs
@@ -4,4 +4,23 @@ using UnityEngine;
 
 public static class ConstValue
 {
+    public static string BattleUIPath = "Prefab/UI/BattleCanvas";
+    public static string CoinUIPath = "Prefab/UI/CoinItem";
+    public static string PopupUIPath = "Prefab/UI/Popup";
+
+    public static int FirstRound = 7;
+    public static int SecondRound = 19;
+    public static int ThirdRound = 43;
+    public static int ForthRound = 79;
+
+    public static int FirstTurnCost = 1000000;
+    public static int SecondTurnCost = 2000000;
+    public static int ThirdTurnCost = 3000000;
+    public static int ForthTurnCost = 4000000;
+    public static int FifthTurnCost = 5000000;
+    public static int SixthTurnCost = 6000000;
+    public static int SeventhTurnCost = 7000000;
+    public static int EighthTurnCost = 8000000;
+    public static int NinthTurnCost = 9000000;
+    public static int TenthTurnCost = 10000000;
 }
