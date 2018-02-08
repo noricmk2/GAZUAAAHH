85aa6bb00745c0abb36670a3de7a516bca04d917
OhSeungWon <osw132@naver.com>
2018-02-08 15:45:27 +0900

    코인매니저

--- Assets/Scripts/Manager/CoinManager.cs
+++ Assets/Scripts/Manager/CoinManager.cs
@@ -1,39 +1,65 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using SimpleJSON; //json 사용하기 위해 추가
 
 public class CoinManager : MonoSingleton<CoinManager>
 {
     Dictionary<CoinName, Coin> DicCoin = new Dictionary<CoinName, Coin>();
+    Dictionary<string, CoinTemplateData> dicTemplateData = new Dictionary<string, CoinTemplateData>(); //코인정보를 외부에서 받아올 dictionary
 
-    public void TestInit()
+    public void TestInit() //json 데이터를 받아와서 DicCoin에 저장
     {
-        DicCoin[CoinName.NEETCOIN] = new Coin(10000.0f, CoinName.NEETCOIN, new BaseSkill());
-        DicCoin[CoinName.ATHURIUM] = new Coin(1000.0f, CoinName.ATHURIUM, new BaseSkill());
+       
+        
     }
-
     public void CoinInit() //코인 정보 초기화
     {
+        TextAsset coinText = Resources.Load(ConstValue.coinpath) as TextAsset;
+        if (coinText != null)
+        {
+            JSONNode rootNode = JSON.Parse(coinText.text);
+            if (rootNode != null)
+            {
+                JSONNode coinTemPlateNode = rootNode["COIN_TEMPLATE"];
 
+                foreach (KeyValuePair<string, JSONNode> templateNode in coinTemPlateNode)
+                {
+                    dicTemplateData.Add(templateNode.Key, new CoinTemplateData(templateNode.Key, templateNode.Value));
+                }
+            }
+        }
+        foreach (KeyValuePair<string, CoinTemplateData> pair in dicTemplateData)
+        {
+            Coin coin = CreateCoin(pair.Key, pair.Value);
+            DicCoin.Add(coin.Name, coin);
+        }
     }
 
-    public Coin CreateCoin(CoinName name) //이름을 받아오면 외부에서 읽어온 데이터를 토대로 코인 생성
+    public Coin CreateCoin(string name, CoinTemplateData coinData) //이름을 받아오면 외부에서 읽어온 데이터를 토대로 코인 생성
     {
         Coin coin = null;
 
         //TODO 파싱한 데이터를 토대로 스킬, 가격, 총량 결정
         switch (name)
         {
-            case CoinName.NEETCOIN:
+            case "NEETCOIN":
+                coin = new Coin(coinData.dicCoinInfo[CoinInfo.Price], CoinName.NEETCOIN, new BaseSkill());
+                coin.CoinAmount = (int)coinData.dicCoinInfo[CoinInfo.Amount];
                 break;
-            case CoinName.ATHURIUM:
+            case "ATHURIUM":
+                coin = new Coin(coinData.dicCoinInfo[CoinInfo.Price], CoinName.ATHURIUM, new BaseSkill());
+                coin.CoinAmount = (int)coinData.dicCoinInfo[CoinInfo.Amount];
                 break;
-            case CoinName.SUFFLE:
+            case "SUFFLE":
+                coin = new Coin(coinData.dicCoinInfo[CoinInfo.Price], CoinName.SUFFLE, new BaseSkill());
+                coin.CoinAmount = (int)coinData.dicCoinInfo[CoinInfo.Amount];
                 break;
-            case CoinName.AOS:
+            case "AOS":
+                coin = new Coin(coinData.dicCoinInfo[CoinInfo.Price], CoinName.AOS, new BaseSkill());
+                coin.CoinAmount = (int)coinData.dicCoinInfo[CoinInfo.Amount];
                 break;
         }
-
         return coin;
     }
 
---
+++ Assets/Scripts/Object/Coin/CoinTemplateData.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SimpleJSON;
+
+public class CoinTemplateData {
+
+    string _strKey = string.Empty;
+    public string strKey { get { return _strKey; } }
+
+    Dictionary<CoinInfo, float> _dicCoinInfo = new Dictionary<CoinInfo, float>();
+    public Dictionary<CoinInfo, float> dicCoinInfo { get { return _dicCoinInfo; } } 
+
+    public CoinTemplateData(string strKey, JSONNode nodeData)
+    {
+        _strKey = strKey;
+
+        for(int i = 0;i<(int)CoinInfo.MAX;i++)
+        {          
+            CoinInfo coinInfo = (CoinInfo)i;
+            float valueData = nodeData[coinInfo.ToString("F")].AsFloat;
+            _dicCoinInfo[coinInfo] = valueData;
+            //Amount 안들어가는거 확인 후 수정
+
+        }
+    } 
+}
